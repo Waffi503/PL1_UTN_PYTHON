43760f0fa110a9a0a8b9c5bc31e430a6eaef4bbe
Waffi503 <carlosbrandan503@gmail.com>
2022-08-30 17:39:33 -0300

    Ejercicio2.py done

--- clase 1/Ejercicio2.py
+++ clase 1/Ejercicio2.py
@@ -13,17 +13,18 @@
 from ast import While
 
 
-continuar = input("")
 print("Bievenido al programa")
-
-while (continuar == "si"):
+pagar_bruto_total = 0
+total_kilos = 0
+i = 1
+while (continuar != "no"):
 
     peso = input("Ingrese el peso (entre 10kg y 100kg) \n ")
     peso = float(peso)
 
     while(peso < 10 or peso > 100):
-            peso = input("ERROR, Ingrese un peso valido (entre 10kg y 100kg) \n ")
-            peso = float(peso)
+        peso = input("ERROR, Ingrese un peso valido (entre 10kg y 100kg) \n ")
+        peso = float(peso)
 
     precio = input("Ingrese el precio por kilo \n ")
     precio = float(precio)
@@ -32,10 +33,49 @@ while (continuar == "si"):
         precio = input("ERROR, Ingrese un precio valido \n ")
         precio = float(precio)
 
-    tipo = input('Ingrese el tipo de producto ("v", "a", "m");(vegetal, animal, mezcla)')
+    tipo = input('Ingrese el tipo de producto ("v", "a", "m");(vegetal, animal, mezcla) \n')
     
     while (tipo != "a" and tipo != "v" and tipo != "m" ):
-        tipo = input('ERROR, Ingrese el tipo de producto ("v", "a", "m");(vegetal, animal, mezcla)')
+        tipo = input('ERROR, Ingrese el tipo de producto ("v", "a", "m");(vegetal, animal, mezcla) \n')
+
+    if (i == 1 or precio > precio_mas_alto):
+        precio_mas_alto = precio
+        precio_mas_alto_tipo = tipo
+    
+    total_kilos += peso
+    pagar_bruto_total += precio * peso
+    i += 1
+    continuar = input('Quiere continuar ? Ingres "no" para salir del programa \n')
+
+print('\n')
+print('\n')
+print('\n')
+print('\n')
+print('El importe total bruto es: ' + str(pagar_bruto_total) + '$')
+
+if total_kilos > 300:
+    pagar_descuento = pagar_bruto_total * 0.75
+    print('El precio a pagar con descuento es: ' + str(pagar_descuento) + '$')
+elif total_kilos > 100:
+    pagar_descuento = pagar_bruto_total * 0.85
+    print('El precio a pagar con descuento es: ' + str(pagar_descuento) + '$')
+else:
+    print('No hay descuento :(')
+
+
+if precio_mas_alto_tipo == "a":
+    print('El tipo mas caro es  Animal')
+elif precio_mas_alto_tipo == "m":
+    print('El tipo mas caro es Mezcla')
+else:
+    print('El tipo mas caro es vegetal')
+
+promedio_kilo = pagar_bruto_total / total_kilos
+
+print('El promedio por kilo es: ' +str(promedio_kilo) + '$')
+
+    
+    
 
 
 
